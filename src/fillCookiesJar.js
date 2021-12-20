62916200bb2aa8012153720ecf529a2b9a98d038
Miguel Duarte <>
2021-12-20 11:00:53 +0000

    More robust scraping

--- src/fillCookiesJar.js
+++ src/fillCookiesJar.js
@@ -1,68 +1,77 @@
 const createBrowser = require('./createBrowser');
-const { Cookie } = require('tough-cookie');
+const {Cookie} = require('tough-cookie');
 const handleCaptcha = require('./handleCaptcha');
-const { isCloudflareJSChallenge, isCloudflareCaptchaChallenge } = require('./utils');
+const {isCloudflareJSChallenge, isCloudflareCaptchaChallenge} = require('./utils');
 const DEFAULT_EXPIRATION_TIME_IN_SECONDS = 3000;
 
 function convertCookieToTough(cookie) {
-  const { name, value, expires, domain, path } = cookie;
-  const isExpiresValid = expires && typeof expires === 'number';
+    const {name, value, expires, domain, path} = cookie;
+    const isExpiresValid = expires && typeof expires === 'number';
 
-  const expiresDate = isExpiresValid
-    ? new Date(expires * 1000)
-    : new Date(Date.now() + DEFAULT_EXPIRATION_TIME_IN_SECONDS * 1000);
+    const expiresDate = isExpiresValid
+        ? new Date(expires * 1000)
+        : new Date(Date.now() + DEFAULT_EXPIRATION_TIME_IN_SECONDS * 1000);
 
-  return new Cookie({
-    key: name,
-    value,
-    expires: expiresDate,
-    domain: domain.startsWith('.') ? domain.substring(1) : domain,
-    path
-  });
+    return new Cookie({
+        key: name,
+        value,
+        expires: expiresDate,
+        domain: domain.startsWith('.') ? domain.substring(1) : domain,
+        path
+    });
 }
 
 async function fillCookiesJar(request, options) {
-  let { jar, url, uri } = options;
-  url = url || uri;
+    let {jar, url, uri} = options;
+    url = url || uri;
 
-  const browser = await createBrowser(options);
-  try {
-    const page = await browser.newPage();
-    let response = await page.goto(url, {
-      timeout: 45000,
-      waitUntil: 'domcontentloaded'
-    });
+    const browser = await createBrowser(options);
+    try {
+        const page = await browser.newPage();
+        let response = await page.goto(url, {
+            timeout: 45000,
+            waitUntil: 'domcontentloaded'
+        });
 
-    let count = 1;
-    let content = await page.content();
+        let count = 1;
+        let content = await page.content();
 
-    while (isCloudflareJSChallenge(content)) {
-      response = await page.waitForNavigation({
-        timeout: 45000,
-        waitUntil: 'domcontentloaded'
-      });
-      content = await page.content();
-      if (count++ === 100) {
-        throw new Error('timeout on just a moment');
-      }
+        while (isCloudflareJSChallenge(content)) {
+            response = await page.waitForNavigation({
+                timeout: 45000,
+                waitUntil: 'domcontentloaded'
+            });
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+            content = await page.content();
+            if (count++ === 100) {
+                throw new Error('timeout on just a moment');
+            }
+        }
+        if (isCloudflareCaptchaChallenge(content)) {
+            await handleCaptcha(content, request, options);
+        }
+
+        const cookies = await page.cookies();
+        for (let cookie of cookies) {
+            jar.setCookie(convertCookieToTough(cookie), url);
+        }
+
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+            content = await page.content();
+            await page.select('#table-apps_length select', "5000");
+            await new Promise((resolve) => setTimeout(resolve, 10000));
+            content = await page.content();
+        } catch (err) {
+            console.log(err);
+        }
+
+        return content;
+    } catch (err) {
+        console.log(err);
+    } finally {
+        await browser.close();
     }
-    if (isCloudflareCaptchaChallenge(content)) {
-      await handleCaptcha(content, request, options);
-    }
-
-    const cookies = await page.cookies();
-    for (let cookie of cookies) {
-      jar.setCookie(convertCookieToTough(cookie), url);
-    }
-
-    await page.select('#table-apps_length select', "5000");
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    content = await page.content();
-
-    return content;
-  } finally {
-    await browser.close();
-  }
 }
 
 module.exports = fillCookiesJar;
